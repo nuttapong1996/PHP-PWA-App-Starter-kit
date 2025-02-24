95f0593bd22d9247fcc03b9ca1c3fe6a51e99310
N0m@d-Xii <34090451+nuttapong1996@users.noreply.github.com>
2025-02-24 23:29:58 +0700

    Update VAPID.php

--- VAPID.php
+++ VAPID.php
@@ -1,25 +1,5 @@
 <?php
 require_once("vendor/autoload.php");
 use Minishlink\WebPush\VAPID;
-
-// var_dump(VAPID::createVapidKeys());
-
-if(file_exists('includes/key/public_key.text') && file_exists('includes/key/private_key.text')){
-    echo "<script>
-            alert('VAPID keys already exist');
-            setTimeout(()=>{
-                window.location.href = 'index.php';
-            },0);
-        </script>";
-}else{
-    file_put_contents('includes/key/public_key.text', VAPID::createVapidKeys()['publicKey']);
-    file_put_contents('includes/key/private_key.text', VAPID::createVapidKeys()['privateKey']);
-    echo "<script>
-            alert('VAPID keys created successfully and saved in includes/key folder');
-            setTimeout(()=>{
-                window.location.href = 'index.php';
-            },0);
-        </script>";
-}
-
-
+header('Content-Type: text/plain');
+var_dump(VAPID::createVapidKeys());
